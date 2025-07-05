d82e24e8b3f2171894e0d4829b709796f85c735e
Suraj Lokhande <surajglokhande@gmail.com>
2025-07-05 11:29:15 +0530

    Update Contents.swift

--- SwiftPlayground.playground/Pages/more patterns.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/more patterns.xcplaygroundpage/Contents.swift
@@ -11,6 +11,31 @@ func Demo() {
     }
 }
 //Demo()
+/*:
+ ## Write a Program where Filter the Man class where addressLine != nil && addressType == "permanent" in Address class
+ */
+struct Address {
+    var addressLine: String?
+    var addressType: String
+}
+
+struct Man {
+    var name: String
+    var address: [Address]?
+}
+
+var arrayAdd = [Address(addressLine: nil, addressType: "permanent"),
+             Address(addressLine: "african", addressType: "permanent"),
+             Address(addressLine: "american", addressType: "temp")]
+
+var arrayPer = [Man(name: "suraj", address: [arrayAdd[0],arrayAdd[1]]),
+                Man(name: "parag", address: [arrayAdd[1],arrayAdd[2]]),
+                Man(name: "vaibhav", address: [arrayAdd[0]])]
+
+var newArray = arrayPer.filter({ $0.address?.filter({ $0.addressLine != nil && $0.addressType == "permanent" }).count ?? 0 > 0 }).map({ $0.name })
+
+print(newArray)
+
 func giveOutput() {
     var array = [1, 2, 3]
     print(array.map { [$0, $0] })//  = [1,1])
