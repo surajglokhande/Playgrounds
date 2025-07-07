7e2c245805ff0d0deec72513ba80b76d569b39d6
surajglokhande <surajglokhande@gmail.com>
2025-07-07 20:11:55 +0530

    - added solutuon of LSP example

--- SwiftPlayground.playground/Pages/Swift_SOLID_LSP.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/Swift_SOLID_LSP.xcplaygroundpage/Contents.swift
@@ -46,4 +46,45 @@ func main() {
 		// As a rectangle we should expect the area as 7 x 5 = 35, but we got 5 x 5 = 25
 }
 main()
+/*:
+    Solution: LSP-compliant design
+ */
+protocol Shape {
+    func area() -> Int
+}
+
+class RectangleLSP: Shape {
+    var width: Int
+    var height: Int
+
+    init(width: Int, height: Int) {
+        self.width = width
+        self.height = height
+    }
+
+    func area() -> Int {
+        return width * height
+    }
+}
+
+class SquareLSP: Shape {
+    var side: Int
+
+    init(side: Int) {
+        self.side = side
+    }
+
+    func area() -> Int {
+        return side * side
+    }
+}
+
+func mainLSP() {
+    let rectangle: Shape = RectangleLSP(width: 7, height: 5)
+    let square: Shape = SquareLSP(side: 5)
+
+    print(rectangle.area()) // 35
+    print(square.area())    // 25
+}
+mainLSP()
 //: [Next](@next)
