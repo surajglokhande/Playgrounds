af002e7a754f09786b29f7bea20b406f4b7a732b
surajglokhande <surajglokhande@gmail.com>
2025-09-03 09:51:24 +0530

    - made changes in for loops

--- SwiftPlayground.playground/Pages/Swift_basic - For Loop.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/Swift_basic - For Loop.xcplaygroundpage/Contents.swift
@@ -7,18 +7,18 @@ func demo() {
 //    for item in stride(from: 8, through: 0, by: -1) {
 //        print(str[item])
 //    }
-    print("\n")
-    for item in stride(from: 0, through: 8, by: 1) {
-        print(str[item])
-    }
-    print("\n")
-    for item in stride(from: 0, to: 8, by: 1) {
-        print(str[item])
-    }
-    
-//    for index in 0...5 {
-//        print(str[index])
+//    print("\n")
+//    for item in stride(from: 0, through: 8, by: 1) {
+//        print(str[item])
 //    }
+//    print("\n")
+//    for item in stride(from: 0, to: 8, by: 1) {
+//        print(str[item])
+//    }
+    
+    for index in stride(from: str.count - 1, through: 0, by: -1) {
+        print(str[index])
+    }
 }
 demo()
 
